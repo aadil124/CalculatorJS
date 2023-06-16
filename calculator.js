fabb3b78953e0cd4900a0ac7d047d2a58bd0344e
aadil124 <ansariaadil124@gmail.com>
2023-06-16 12:00:47 +0530

    added functionality

---
+++ calculator.js
@@ -0,0 +1,99 @@
+//create container element using DOM
+let container = document.createElement("div");
+
+container.setAttribute(
+  "class",
+  "container d-flex flex-column flex-content-center"
+);
+
+container.innerHTML = `
+    <h1 id="title" class="text-center">Calculator</h1>
+ <p id="description" class="text-center">Perform All you calculation at one place</p>
+
+
+<!--- Calcultor Section --->
+<div class="calculator">
+<input type="text"  placeholder="0" id="result"  onkeypress="return isNumberkey(event)">
+<button onclick="clr()" id="clear">AC</button>
+<button onclick="del()" id="delete">DEL</button>
+<button onclick="display('%')" id="modulus">%</button>
+<button onclick="display('/')" id="divide">/</button>
+<button onclick="display('7')" id="7">7</button>
+<button onclick="display('8')" id="8">8</button>
+<button onclick="display('9')" id="9">9</button>
+<button onclick="display('*')" id="multiply">*</button>
+<button onclick="display('4')" id="4">4</button>
+<button onclick="display('5')" id="5">5</button>
+<button onclick="display('6')" id="6">6</button>
+<button onclick="display('-')" id="subtract">-</button>
+<button onclick="display('1')" id="1">1</button>
+<button onclick="display('2')" id="2">2</button>
+<button onclick="display('3')" id="3">3</button>
+<button onclick="display('+')" id="add">+</button>
+<button onclick="display('.')" id="decimal">.</button>
+<button onclick="display('0')" id="0">0</button>
+<button class="equalBtn" onclick="calculate('=')" id="equal">=</button>
+</div>
+
+
+<div id="restrictToast">
+alert("Only Numbers are allowed");
+</div>
+
+
+<div id="invalidToast">
+alert("Please enter a valid expression");
+
+</div>
+
+
+
+`;
+
+document.body.append(container);
+
+let outputDisplay = document.getElementById("result");
+
+//Function to restrict user to not allow to enter any keys otherthan number.
+function isNumberkey(event) {
+  let restrictElement = document.getElementById("restrictToast");
+
+  let restrictToast = new bootstrap.Toast(restrictElement, {
+    delay: 3000,
+  });
+
+  let charcode = event.which ? event.which : event.keyCode;
+  if (charcode > 31 && (charcode < 48 || charcode > 57)) {
+    restrictToast.show();
+    return false;
+  } else return true;
+}
+
+//function to diaply the value in input box
+function display(val) {
+  if (val === "." && outputDisplay.value.includes(".")) {
+    return;
+  } else outputDisplay.value += val;
+}
+
+//function to evaluate the data
+function calculate() {
+  let invalidElement = document.getElementById("invalidToast");
+  let invalidToast = new bootstrap.Toast(invalidElement, {
+    delay: 3000,
+  });
+
+  try {
+    outputDisplay.value = eval(outputDisplay.value).toFixed(3);
+  } catch (error) {
+    invalidToast.show();
+  }
+}
+
+function clr() {
+  outputDisplay.value = "";
+}
+
+function del() {
+  outputDisplay.value = outputDisplay.value.slice(0, -1);
+}
